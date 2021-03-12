2cc4b7b19f45e14aa41588ce5fc33b25d64138c1
de_gentlem4n <th3g3n3lm4n@gmail.com>
2021-03-11 21:13:12 -0500

    add changed

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 require('dotenv').config()
+// helps link files found in node
+const path = require('path')
 const express = require('express')
 
 const server = express()
 
 // middle ware
 server.use(express.json())
+// finds static assets -> tells express where our static assets are serverd from
+server.use(express.static(path.join(__dirname, 'client/build', 'index.html')))
 
 console.log(process.env.NODE_ENV)
 if(process.env.NODE_ENV === 'development'){
@@ -12,8 +16,12 @@ if(process.env.NODE_ENV === 'development'){
   server.use(cors())
 }
 
+server.get('/api/hello', (req,res) => {
+  res.json({message: 'hello world hoee'})
+})
+
 server.use('*', (req, res) => {
-  res.send('<h1>succ beans</h2>')
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 
 
